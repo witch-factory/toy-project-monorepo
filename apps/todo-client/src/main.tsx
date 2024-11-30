709171bc9d011ec3d77a75a690bdc783737bb0ab
witch-factory <soakdma37@gmail.com>
2024-11-30 21:18:54 +0900

    client react router 기본 세팅

--- apps/todo-client/src/main.tsx
+++ apps/todo-client/src/main.tsx
@@ -2,10 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./App.css";
 import App from "./App.tsx";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 // biome-ignore lint/style/noNonNullAssertion: This is library code
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+			</Routes>
+		</BrowserRouter>
 	</StrictMode>,
 );
