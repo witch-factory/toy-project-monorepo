60461279482817b122f674e93d7f4b0a2b8dd576
witch-factory <soakdma37@gmail.com>
2024-12-01 21:28:02 +0900

    nestjs swagger 라이브러리 설치, 기본 설정

--- apps/todo-server/src/main.ts
+++ apps/todo-server/src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors({
 		origin: "http://localhost:5173",
 	});
+
+	// Swagger 설정
+	const config = new DocumentBuilder()
+		.setTitle("Todo API")
+		.setDescription("Todo CRUD API documentation")
+		.setVersion("1.0")
+		.addTag("todos") // 태그 추가
+		.build();
+
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("api-docs", app, documentFactory);
+
 	await app.listen(3000);
 }
 bootstrap();
