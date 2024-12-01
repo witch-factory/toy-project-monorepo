93e8963dc7eaf986b6fe42503eaddd7b2abaa32b
witch-factory <soakdma37@gmail.com>
2024-12-01 23:41:14 +0900

    entity를 정의하고 이를 통해 api swagger 문서 생성

--- apps/todo-server/src/todos/entities/todo.entity.ts
+++ apps/todo-server/src/todos/entities/todo.entity.ts
@@ -1,1 +1,10 @@
-export class Todo {}
+import { Todo } from "@prisma/client";
+
+export class TodoEntity implements Todo {
+	id: number;
+	title: string;
+	userId: number;
+	completed: boolean;
+	createdAt: Date;
+	updatedAt: Date;
+}
--- apps/todo-server/src/todos/todos.controller.ts
+++ apps/todo-server/src/todos/todos.controller.ts
@@ -15,24 +15,59 @@ import {
 import { TodosService } from "./todos.service";
 import { CreateTodoDto } from "./dto/create-todo.dto";
 import { UpdateTodoDto } from "./dto/update-todo.dto";
+import {
+	ApiBody,
+	ApiOperation,
+	ApiParam,
+	ApiQuery,
+	ApiResponse,
+	ApiTags,
+} from "@nestjs/swagger";
+import { Todo, User } from "@prisma/client";
+import { TodoEntity } from "./entities/todo.entity";
 
+@ApiTags("todos")
 @Controller("todos")
 export class TodosController {
 	constructor(private readonly todosService: TodosService) {}
 
 	@Post()
+	@ApiOperation({ summary: "새로운 todo 생성" }) // 엔드포인트 요약
+	@ApiBody({ type: CreateTodoDto })
+	@ApiResponse({
+		status: 201,
+		description: "Todo created successfully",
+		type: TodoEntity,
+	})
 	@UsePipes(new ValidationPipe({ transform: true }))
 	create(@Body() createTodoDto: CreateTodoDto) {
 		return this.todosService.create(createTodoDto);
 	}
 
 	@Get()
-	async findAll(@Query("userId", ParseIntPipe) userId: number) {
+	@ApiOperation({ summary: "주어진 사용자의 todo 조회" })
+	@ApiQuery({ name: "userId", type: "number" })
+	@ApiResponse({
+		status: 200,
+		description: "Todo list retrieved successfully",
+		type: [TodoEntity],
+	})
+	async findAll(
+		@Query("userId", ParseIntPipe) userId: number,
+	): Promise<Todo[]> {
 		const todos = await this.todosService.findAllByUserId(userId);
 		return todos;
 	}
 
 	@Patch(":todoId")
+	@ApiOperation({ summary: "todo 수정" })
+	@ApiParam({ name: "todoId", type: "number" })
+	@ApiBody({ type: UpdateTodoDto })
+	@ApiResponse({
+		status: 200,
+		description: "Todo updated successfully",
+		type: TodoEntity,
+	})
 	async update(
 		@Param("todoId", ParseIntPipe) todoId: number,
 		@Body(new ValidationPipe({ transform: true })) updateTodoDto: UpdateTodoDto,
