23a05d51865a4ea34a56fed50ffd9c253e4a86ac
witch-factory <soakdma37@gmail.com>
2024-12-03 18:30:26 +0900

    메인 모듈에서 prisma service 삭제

--- apps/todo-server/src/app.module.ts
+++ apps/todo-server/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from "@nestjs/common";
 import { TodosModule } from "./todos/todos.module";
 import { UsersModule } from "./users/users.module";
-import { PrismaService } from "./prisma.service";
 
 @Module({
 	imports: [TodosModule, UsersModule],
-	providers: [PrismaService],
 })
 export class AppModule {}
